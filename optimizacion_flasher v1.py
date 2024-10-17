b1eb1a77c753a99f5d02826c998bf7449de42961
Karen Quezada <karen.soledad.quezada@gmail.com>
2024-10-17 00:59:15 -0300

    avance en optimizacion y nueva iteracion de grafico

--- optimizacion_flasher v1.py
+++ optimizacion_flasher v1.py
@@ -9,64 +9,16 @@ delta = np.pi / 3.7
 alpha_variable = np.pi / 2 - np.pi / m
 epsilon = 10 * np.pi / 250
 eta_variable = (np.pi / m) - (epsilon / 2)
-h = 35
+h = 31
 distancia_k = dk(h, epsilon, m)
 grosor=0.4 #corregir sumando parte de los pliegues -quizas-
-
-def N(r, s_f):
-    #calcula el numero de puntos p(i,j)
-    r=int(r)
-    sum_d = sum(e_distance(i, i + 1) for i in range(r))
-    return int((s_f * sum_d) / distancia_k)
-
-r_i_j_cache = {}
-def r_i_j(params):
-    params_tuple = tuple(params)
-    
-    if params_tuple in r_i_j_cache:
-        return r_i_j_cache[params_tuple]
-
-    i, j, m, i_final, delta, alpha_variable, epsilon, altura, s_f = params  # Añadir s_f a los parámetros
-    i_final = int(i_final)
-    resultados = []
-    distancia_k = dk(altura, epsilon, m)
-    punto_inicial = p(0, j, m, delta, alpha_variable, epsilon, s_f)  # Pasar s_f a p
-    if np.iscomplexobj(punto_inicial):
-        print(f"punto_inicial es complejo: {punto_inicial}")
-    punto_actual = punto_inicial 
-    modulos_totales = lista_modulos(j, i_final, m, delta, alpha_variable, epsilon, distancia_k, s_f)[0]
-    total_modulos = lista_modulos(j, i_final, m, delta, alpha_variable, epsilon, distancia_k, s_f)[2]
-    
-    if total_modulos < distancia_k:
-        punto_actual = punto_inicial
-    else:
-        indice = encontrar_indice(modulos_totales, distancia_k * i)
-        if indice == -1:
-            return None
-        
-        remanente = distancia_k * i - sum(modulos_totales[:indice])
-        resultados.append([indice, remanente])
-
-        for idx, remanente in resultados:
-            if idx >= i:
-                break
-        p_start = p(idx, j, m, delta, alpha_variable, epsilon, s_f)  # Pasar s_f a p
-        p_next = p(idx + 1, j, m, delta, alpha_variable, epsilon, s_f)  # Pasar s_f a p
-        direccion = (p_next - p_start) / e_distance(p_next, p_start)
-        desplazamiento = direccion * remanente
-        punto_actual = p_start + desplazamiento
-        if np.iscomplexobj(punto_actual):
-            print(f"punto_actual es complejo: {punto_actual}")
-    r_i_j_cache[params_tuple] = punto_actual
-    return punto_actual
 
 # Definir la función objetivo a maximizar
 def objective(individual):
     """distancia euclidiana hasta un punto final que define desde 
     un extremo a otro (diametro, pero no área de circulo)"""
     r, sf= individual
-    n = N(r, sf)
-    m_Njx, m_Njy = r_i_j((n, 0, m, r, delta, alpha_variable, epsilon, h, sf))  
+    m_Njx, m_Njy = p(r, 0, m, delta, alpha_variable, epsilon, sf) 
     return 2 * np.sqrt(m_Njx**2 + m_Njy**2)  
 
 def constraint1(individual):
@@ -74,8 +26,8 @@ def constraint1(individual):
     diametroplegado = diametro_plegado(r, 0, m, delta, alpha_variable, epsilon, h, sf, grosor)
     
     # Si el diámetro plegado excede 64, penalizar fuertemente
-    if diametroplegado > 60:
-        return (diametroplegado - 60) ** 3  # Penalización cuadrática para valores mayores a 64
+    if diametroplegado > 61:
+        return (diametroplegado - 61) ** 3  # Penalización cuadrática para valores mayores a 64
     else:
         return 0  # No hay penalización si está dentro del límite
 
@@ -99,7 +51,7 @@ def evaluate(individual):
 def apply_constraints(individual):
     # Aquí se pueden aplicar las restricciones específicas, por ejemplo:
     individual[0] = int(round(individual[0]))  # Redondear r
-    individual[0] = max(1, min(individual[0], 4))  # Asegurar que esté entre 1 y 3
+    individual[0] = max(1, min(individual[0], 5))  # Asegurar que esté entre 1 y 3
     sf_lower_bound = 0.0001
     sf_upper_bound = 80
 
@@ -122,7 +74,7 @@ def generate_positive_sf():
     return random.uniform(0.0001, 80)  # Genera un número en el rango
 
 # Atributos: m_N,j,x, m_N,j,y son continuos, r es discreto
-toolbox.register("attr_r", random.randint, 0, 4)
+toolbox.register("attr_r", random.randint, 1, 5)
 toolbox.register("attr_sf", generate_positive_sf)
 
 # Un individuo es una lista de esos atributos
